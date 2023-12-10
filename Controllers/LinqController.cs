31700531d5d71545d301002002e97e39e878a73c
markieboy223 <markvanderburg01@gmail.com>
2023-12-10 13:11:18 +0100

    queries

--- Controllers/LinqController.cs
+++ Controllers/LinqController.cs
@@ -73,5 +73,87 @@ namespace WebApplication1.Controllers
             var students = await _context.Student.OrderByDescending(s => s.Leeftijd).ToListAsync();
             return students;
         }
+
+        [HttpGet("NaamEnLeeftijdStudent")]
+        public async Task<IEnumerable<object>> NaamEnLeeftijdStudent()
+        {
+            var students = await _context.Student.Select(s => new { Naam = s.Name, Leeftijd = s.Leeftijd }).ToListAsync();
+            return students;
+        }
+
+        [HttpGet("VakkenMetCijferHogerDan8")]
+        public async Task<IEnumerable<Vak>> VakkenMetCijferHogerDan8()
+        {
+            var vakken = await _context.Vakken.Where(v => v.Cijfer > 8).ToListAsync();
+            return vakken;
+        }
+
+        [HttpGet("DocentDieLesGeeft")]
+        public async Task<IEnumerable<Docent>> DocentDieLesGeeft()
+        {
+            var docenten = await _context.Docent.Where(d => d.Eigenschap.Equals("Teaching")).ToListAsync();
+            return docenten;
+        }
+
+        [HttpGet("StudentenMetElectricalEngineering")]
+        public async Task<IEnumerable<object>> StudentenMetElectricalEngineering()
+        {
+            var student = await _context.Student
+                .Where(s => s.Opleiding.Name == "Electrical Engineering")
+                .ToListAsync();
+            return student;
+        }
+
+        [HttpGet("GemiddeldCijferPerStudent")]
+        public async Task<IEnumerable<object>> GemiddeldCijferPerStudent()
+        {
+            var gemiddelde = await _context.Student
+                .GroupBy(s => new { s.Name, s.OpleidingId})
+                .Select(g => new
+                {
+                    Naam = g.Key.Name,
+                    OpleidingId = g.Key.OpleidingId,
+                    GemiddeldCijfer = g.Join(_context.Vakken, s => s.OpleidingId, v => v.OpleidingId, (s, v) => v.Cijfer).Average()
+                })
+                .ToListAsync();
+            return gemiddelde;
+        }
+
+        [HttpGet("vakkenBusinessOpleiding")]
+        public async Task<IEnumerable<Vak>> vakkenBusinessOpleiding()
+        {
+            var vakken = await _context.Vakken.Where(v => v.OpleidingId == 3).ToListAsync();
+            return vakken;
+        }
+
+        [HttpGet("DocentenJongerDan40")]
+        public async Task<IEnumerable<object>> DocentenJongerDan40()
+        {
+            var docenten = await _context.Docent
+                .Select(s => new {Naam = s.Name, Leeftijd = s.Leeftijd})
+                .Where(d => d.Leeftijd < 40)
+                .ToListAsync();
+            return docenten;
+        }
+        [HttpGet("StudentOuderDan20Cityville")]
+        public async Task<IEnumerable<string>> StudentOuderDan20Cityville()
+        {
+            var studenten = await _context.Student.Where(s => s.Leeftijd > 20 && s.Woonplaats.Equals("Cityville")).Select(v => v.Name).ToListAsync();
+            return studenten;
+        }
+
+        [HttpGet("vakkenCijfer")]
+        public async Task<IEnumerable<Vak>> vakkenCijfer()
+        {
+            var vakken = await _context.Vakken.Where(v => v.Cijfer > 7.5 && v.Cijfer < 9).ToListAsync();
+            return vakken;
+        }
+
+        [HttpGet("DocentResearch")]
+        public async Task<IEnumerable<string>> DocentResearch()
+        {
+            var docenten = await _context.Docent.Where(d => d.Eigenschap.Equals("Research")).Select(n => n.Name).ToListAsync();
+            return docenten;
+        }
     }
 }
--- Models/Opleiding.cs
+++ Models/Opleiding.cs
@@ -1,7 +1,10 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Opleiding
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Student> studenten {  get; set; }
